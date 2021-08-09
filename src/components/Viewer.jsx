7759685a07f0f3c729ab8eb2920c7fa7de9f62db
Yusy <yusay.safadi@gmail.com>
2021-08-09 15:19:14 +0200

    added a catalog of stickers inside the viewer component with a preview of the sticker (will encapsulate soon in a separate commpinent)

--- src/components/Viewer.jsx
+++ src/components/Viewer.jsx
@@ -1,21 +1,43 @@
-import React,{useState} from 'react'
-
+import React,{useState, useEffect} from 'react'
+import data from "../data"
 export const Viewer =() =>{
-    const [currentSticker, setCurrentSticker] = useState("")
     const[inputValue, setInputValue] = useState("")
-
-    const handleChange = (e) =>{
+    const [currentSticker, setCurrentSticker] = useState()
+    
+    const handleChange = (e) =>{ 
+        let searchInput = e.target.value
+        let result = data.find(({name})=> name.toLowerCase() === searchInput.toLowerCase())
         setInputValue(e.target.value)
+        if(result !== undefined){
+            setCurrentSticker(result.src)
+        }
+        
+
     }
     return (
+        <>
         <div>
-        <picture>
+       
         
-        <img src={currentSticker} alt="aweawea" type="image/webp" />
-        </picture>
+        <img src={currentSticker} alt="aweawea" />
+        
             
             <input type="text" name="" id="" value={inputValue} onChange={handleChange}  />
         </div>
+
+        <div>
+            <h2>Catalog</h2>
+            {data.map((sticker, index) =>{
+                return <ul>
+                <div>
+                <img src={sticker.src} alt="aweawea" />
+                <li key={index}>{sticker.name}</li>
+                </div>
+                </ul>
+            })}
+
+        </div>
+        </>
     )
 }
 
