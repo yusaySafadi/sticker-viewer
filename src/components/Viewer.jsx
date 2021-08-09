521bc3a3187047691b3283d72784e1f7427c1fe8
Yusy <yusay.safadi@gmail.com>
2021-08-09 13:50:11 +0200

    Webp images are now loadable in Viewer component

--- src/components/Viewer.jsx
+++ src/components/Viewer.jsx
@@ -2,14 +2,18 @@ import React,{useState} from 'react'
 
 export const Viewer =() =>{
     const [currentSticker, setCurrentSticker] = useState("")
-    const[inputValue, setInputValue] = useState("Nase")
+    const[inputValue, setInputValue] = useState("")
 
     const handleChange = (e) =>{
         setInputValue(e.target.value)
     }
     return (
         <div>
-            <img src={currentSticker} alt="" />
+        <picture>
+        
+        <img src={currentSticker} alt="aweawea" type="image/webp" />
+        </picture>
+            
             <input type="text" name="" id="" value={inputValue} onChange={handleChange}  />
         </div>
     )
